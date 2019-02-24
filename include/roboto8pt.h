ddd8bc2bbc7a8705495e8872938593467c72af37
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-02-24 21:01:32 +0100

    initial commit

---
+++ include/roboto8pt.h
@@ -0,0 +1,7 @@
+// Font data for Roboto 8pt
+#include <stdint.h>
+#include "nrf_font.h"
+extern const uint8_t roboto_8ptBitmaps[];
+extern const FONT_INFO roboto_8ptFontInfo;
+extern const FONT_CHAR_INFO roboto_8ptDescriptors[];
+
